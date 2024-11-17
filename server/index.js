18e6ebb8ff88624ae69063da4bc23cdaf98076bd
Kozak <474kozak@gmail.com>
2024-11-17 13:15:31 +0300

    Product

--- server/index.js
+++ server/index.js
@@ -18,6 +18,16 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client-build')));
+
+  // Любой маршрут, кроме API, должен возвращать index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client-build', 'index.html'));
+  });
+}
+
 // Обработка ошибок (последний Middleware)
 app.use(errorHandler);
 
